687ac44b02cbccd4df38594cf438b58b6fbebe39
Gaurid123 <gaurideshmukhofficial@gmail.com>
2024-11-27 15:55:39 +0530

    by piyusha

---
+++ newconfige-client2/src/main/java/com/cjc/main/NewconfigeClient2Application.java
@@ -0,0 +1,16 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NewconfigeClient2Application {
+
+	public static void main(String[] args)
+	{
+		System.out.println("This is client");
+		SpringApplication.run(NewconfigeClient2Application.class, args);
+	}
+
+}
